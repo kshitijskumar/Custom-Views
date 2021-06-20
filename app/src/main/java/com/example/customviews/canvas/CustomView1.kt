65d52a180634bd1175a9730abe59bfbd770747f0
Kshitij Kumar <kshitij2212@gmail.com>
2021-06-20 18:22:46 +0530

    drawing lined and circle

--- app/src/main/java/com/example/customviews/canvas/CustomView1.kt
+++ app/src/main/java/com/example/customviews/canvas/CustomView1.kt
@@ -1,15 +1,24 @@
 package com.example.customviews.canvas
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 
 //I've divided the various functions in Parts in which I feel you should read, it may be better for understanding (as it was for me)
+//We've multiple options for constructors in a view, we've chosen this constructor rather than the one with only context, is because we want
+//use this view from xml as well, where to set width height and everything we need AttributeSet
 class CustomView1(context: Context, attrs: AttributeSet?) : View(context, attrs) {
 
     companion object {
         private const val TAG = "CustomView1"
     }
+
+    private var finalWidth = 0
+    private var finalHeight = 0
 
     //Part 1
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -25,7 +34,7 @@ class CustomView1(context: Context, attrs: AttributeSet?) : View(context, attrs)
         val heightMode = MeasureSpec.getMode(heightMeasureSpec)
 
         //we set the width of our custom view
-        val finalWidth = when (widthMode) {
+        finalWidth = when (widthMode) {
             //this mode is when you set layout_width as match_parent or some definite dp, eg. 300dp
             //then we don't want to change the size
             MeasureSpec.EXACTLY -> widthSize
@@ -36,7 +45,7 @@ class CustomView1(context: Context, attrs: AttributeSet?) : View(context, attrs)
             else -> 200
         }
 
-        val finalHeight = when (heightMode) {
+        finalHeight = when (heightMode) {
             MeasureSpec.EXACTLY -> heightSize
             MeasureSpec.AT_MOST -> Math.min(200, heightSize)
             else -> 200
@@ -48,5 +57,45 @@ class CustomView1(context: Context, attrs: AttributeSet?) : View(context, attrs)
         //Note: If you do not specify the width of your view and simply use widthSize in setMeasuredDimension,
         //then even if you set the width as wrap_content, your view would  take the entire width of the parent
         //hence if you want some dedicated size in case of wrap_content, do change the width on mode AT_MOST
+    }
+
+    //Part 2
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.STROKE
+        strokeWidth = 10f
+    }
+    private val color1 = Color.CYAN
+
+    //try to avoid as much as calculation out from the onDraw method, because this is called multiple times
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        Log.d(TAG, "onDraw called")
+        if (canvas == null) return
+        invalidate()
+//        drawLine(canvas)
+//        drawDiagonals(canvas)
+        drawDiagonalFlower(canvas)
+    }
+
+    private fun drawLine(canvas: Canvas) {
+        //we draw a horizontal line on top of our view
+        paint.color = color1
+        canvas.drawLine(0f, 0f, (finalWidth / 2).toFloat(), 0f, paint)
+    }
+
+    private fun drawDiagonals(canvas: Canvas) {
+        paint.color = color1
+        canvas.drawLine(0f, 0f, finalWidth.toFloat(), finalHeight.toFloat(), paint)
+        canvas.drawLine(0f, finalHeight.toFloat(), finalWidth.toFloat(), 0f, paint)
+    }
+
+    private fun drawDiagonalFlower(canvas: Canvas) {
+        drawDiagonals(canvas)
+        canvas.drawCircle(
+            finalWidth.toFloat() / 2,
+            finalHeight.toFloat() / 2,
+            finalHeight.toFloat() / 5,
+            paint
+        )
     }
 }
